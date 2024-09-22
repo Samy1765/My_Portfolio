5daf9f6994167550d129ae733944467bd7478f49
Swayam <Swayamdeshmukh1765pgmail.com>
2024-09-22 16:49:39 +0530

    Render Title

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
+
 col1,col2=st.columns(2)
 
 with col1:
-   image=st.image("images for my portfolio/my_img.png",width=400)
+   st.image("images for my portfolio/My_img.png",width=400)
 
 with col2:
    st.title("Swayam Deshmukh")
@@ -13,11 +15,26 @@ Hello! I'm Swayam Deshmukh
 
 Welcome to my corner of the web! I'm a Student in JSPM's RSCOE.
 
-Through my work, I aim to combine creativity with strategy, offering solutions that are both innovative and effective. Whether it's [mention specific services or products you offer], my focus is always on delivering high-quality results that make an impact.
+Through my work, I aim to combine creativity with strategy, offering solutions that are both innovative and effective. Whether it's geting an IT job or doing Business, my focus is always on delivering high-quality results that make an impact.
 
 When I'm not Coding, you can find me Watching Animes, Reading Books.
 
 Feel free to explore my work, and don’t hesitate to get in touch if you'd like to collaborate or learn more!
 """
-   st.write(content)
 
+   st.info(content)
+
+content2="""
+Below you can find some of the app I have Built in Python.  """
+st.write(content2)
+
+col3,col4=st.columns(2)
+
+df=pandas.read_csv("data.csv",sep=";")
+with col3:
+   for index, rows in df[:10].iterrows():
+      st.header(rows["title"])
+
+with col4:
+   for index, rows in df[10:].iterrows():
+      st.header(rows["title"])
